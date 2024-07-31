592afa722a28ccdf9eb473c5c49e41912c36c782
Mustafa Yuksel <mryuksel@gmail.com>
2024-07-31 23:34:54 +0300

    added functionalty to opacity bacground hwn opned menu

--- src/components/hamburger_menu/HamburgerMenu.tsx
+++ src/components/hamburger_menu/HamburgerMenu.tsx
@@ -3,7 +3,6 @@ import { FaBars } from 'react-icons/fa';
 import { IoClose } from 'react-icons/io5';
 import { sideBarListItems } from '../../constants/SideBarListItems';
 import SideBarListItem from '../side_bar/SideBarListItem';
-
 const HamburgerMenu: React.FC = () => {
     const [isOpen, setIsOpen] = useState(false);
 
@@ -16,6 +15,21 @@ const HamburgerMenu: React.FC = () => {
             <button onClick={toggleMenu} className="text-3xl focus:outline-none p-5">
                 {isOpen ? <IoClose /> : <FaBars />}
             </button>
+
+            {/* <div
+                className={`fixed top-0 left-0 w-full h-full bg-black z-40 transition-opacity duration-500 ease-in-out transform ${isOpen ? 'opacity-50 block' : 'opacity-0 hidden'
+                    }`}
+                onClick={toggleMenu}>
+            </div> */}
+
+            <div
+                className={`fixed top-0 left-0 w-full h-full bg-black z-40 transition-opacity duration-500 ease-in-out will-change-transform transform ${isOpen ? 'opacity-50 pointer-events-auto' : 'opacity-0 pointer-events-none'
+                    }`}
+                onClick={
+                    toggleMenu
+                }>
+            </div>
+
             <div
                 className={`fixed top-15 left-0 w-64 h-full bg-gray-100 shadow-lg z-50 transform-gpu duration-300 ease-in-out rounded-r-2xl ${isOpen ? 'translate-x-0' : '-translate-x-full'
                     }`}
