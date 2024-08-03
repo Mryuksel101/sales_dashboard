dac68a31e0915b6f452111c24ce10239c96c8994
Mustafa Yuksel <mryuksel@gmail.com>
2024-08-03 20:34:20 +0300

    pesude element egliştirmesi

--- src/components/hamburger_menu/HamburgerMenu.tsx
+++ src/components/hamburger_menu/HamburgerMenu.tsx
@@ -3,6 +3,7 @@ import { FaBars } from 'react-icons/fa';
 import { IoClose } from 'react-icons/io5';
 import { sideBarListItems } from '../../constants/SideBarListItems';
 import SideBarListItem from '../side_bar/SideBarListItem';
+import './Menu.css';
 
 const HamburgerMenu: React.FC = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -23,7 +24,7 @@ const HamburgerMenu: React.FC = () => {
                 {isOpen ? <IoClose /> : <FaBars />}
             </button>
 
-            {/* <div
+            <div
                 className={`fixed top-0 left-0 inset-0 bg-black transform-cpu transition-opacity ease-in-out duration-300 ${isOpen ? 'opacity-50 pointer-events-auto' : 'opacity-0 pointer-events-none'}`}
                 onClick={toggleMenu}
                 style={{
@@ -31,25 +32,30 @@ const HamburgerMenu: React.FC = () => {
                     backfaceVisibility: 'hidden',
                     zIndex: 150,
                 }}
-            ></div> */}
 
-            <div
-                className={`left-0 w-64 h-full bg-gray-100 rounded-r-2xl ease-in-out duration-300`}
-                style={{
-                    position: 'fixed',
-                    transform: isOpen ? ' translateX(0)' : ' translateX(-100%)',
-                    transition: 'transform 300ms ease-in-out',
-                    //willChange: 'transform',
-                    opacity: 0.99,
-                    backfaceVisibility: 'hidden',
-                    perspective: '1000px',
-                    zIndex: 160,
-                }}
-            >
-                <ul className="flex flex-col p-4 pt-16">
-                    {memoizedItems}
-                </ul>
+            ></div>
+
+            <div className={`menu`}>
+                <div
+                    className={`left-0 w-64 h-full bg-gray-100 rounded-r-2xl ease-in-out duration-300`}
+                    style={{
+                        position: 'fixed',
+                        transform: isOpen ? ' translateX(0)' : ' translateX(-100%)',
+                        transition: 'transform 300ms ease-in-out',
+                        //willChange: 'transform',
+                        opacity: 0.99,
+                        backfaceVisibility: 'hidden',
+                        perspective: '1000px',
+                        zIndex: 160,
+                    }}
+                >
+                    <ul className="flex flex-col p-4 pt-16">
+                        {memoizedItems}
+                    </ul>
+                </div>
             </div>
+
+
         </>
     );
 };
