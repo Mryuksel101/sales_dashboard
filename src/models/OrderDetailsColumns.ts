79c3dd68a7242fa9b5adb9a73e09262bfc518d78
Mustafa Yuksel <mryuksel@gmail.com>
2024-09-26 23:43:12 +0300

    Refactor OrderDetailsColumns.ts: Update column widths and alignments

--- src/models/OrderDetailsColumns.ts
+++ src/models/OrderDetailsColumns.ts
@@ -1,21 +1,19 @@
 import { GridColDef } from '@mui/x-data-grid';
 
 export const orderDetailsColumns: GridColDef[] = [
-    { field: 'stok_Kodu', headerName: 'Stok Kodu', width: 220, align: 'left', },
-    { field: 'aciklama', headerName: 'Açıklama', width: 250, align: 'left' },
-    { field: 'miktar', headerName: 'Miktar', width: 100, align: 'right' },
-    { field: 'teslim_Edilen_Miktar', headerName: 'Teslim Edilen Miktar', width: 160, align: 'right' },
-    { field: 'bekleyen_Miktar', headerName: 'Bekleyen Miktar', width: 160, align: 'right' },
-    { field: 'birim', headerName: 'Birim', width: 80, align: 'center' },
+    { field: 'stok_Kodu', headerName: 'Stok Kodu', width: 250, align: 'left' },
+    { field: 'aciklama', headerName: 'Açıklama', width: 300, align: 'left' },
+    { field: 'miktar', headerName: 'Miktar', width: 120, align: 'right' },
+    { field: 'teslim_Edilen_Miktar', headerName: 'Teslim Edilen Miktar', width: 180, align: 'right' },
+    { field: 'bekleyen_Miktar', headerName: 'Bekleyen Miktar', width: 180, align: 'right' },
+    { field: 'birim', headerName: 'Birim', width: 100, align: 'center' },
     {
         field: 'editableQuantity',
         headerName: 'Adet Gir',
-        width: 130,
+        width: 150,
         editable: true,
         type: 'number',
         align: 'right',
         description: 'Adet Giriniz',
     },
 ];
-
-
